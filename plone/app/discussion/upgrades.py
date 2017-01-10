ad349b36deb66cf7355bd385f34be4ba14186965
Maurits van Rees <maurits@vanrees.org>
2017-01-10 18:09:01 +0100

    Make comment on private content not publicly available in search results.
    This is part of PloneHotfix20161129.
    
    Updated metadata.xml version to 1000.  This leaves more room for
    profile version increases in earlier releases.  We apply the rolemap
    step again to avoid accidentally missing it.

--- plone/app/discussion/upgrades.py
+++ plone/app/discussion/upgrades.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from plone.app.discussion.interfaces import IDiscussionSettings
 from plone.registry.interfaces import IRegistry
+from Products.CMFCore.utils import getToolByName
 from zope.component import getUtility
+
+import logging
 
 
 default_profile = 'profile-plone.app.discussion:default'
+logger = logging.getLogger('plone.app.discussion')
 
 
 def update_registry(context):
@@ -14,3 +18,14 @@ def update_registry(context):
 
 def update_rolemap(context):
     context.runImportStepFromProfile(default_profile, 'rolemap')
+
+
+def upgrade_comment_workflows(context):
+    context.runImportStepFromProfile(default_profile, 'workflow')
+    catalog = getToolByName(context, 'portal_catalog')
+    for brain in catalog(portal_type='Discussion Item'):
+        try:
+            comment = brain.getObject()
+            comment.reindexObjectSecurity()
+        except (AttributeError, KeyError):
+            logger.info('Could not reindex comment %s' % brain.getURL())
